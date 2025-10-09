9c4b1acd9606e9ea1a89bd38a4a8e26db0f1c0ea
Riley <rileythomas.elevate@gmail.com>
2025-10-09 16:34:22 -0400

    added docker container setup for mongo

--- Assignments/Assignment-1/server.js
+++ Assignments/Assignment-1/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/UserRoutes');
 const employeeRoutes = require('./routes/EmployeeRoutes');
 
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/comp3123-assignment1';
 const PORT = process.env.PORT || 8089;
+
 
 const app = express();
 app.use(express.json());
@@ -12,15 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
-// All the database connection stuff
-connectDB(DB_URL)
-    .then(() => {
-        console.log('Successfully connected to MongoDB');
-        app.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Could not connect to the database.', err);
-        process.exit(1);
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to the database');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
+}).catch(err => {
+    console.error('Database connection error:', err);
+    process.exit(1);
+});
