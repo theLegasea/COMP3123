bc082ee515701270b1817c6f8feabd5ae1f49230
Riley <rileythomas.elevate@gmail.com>
2025-10-09 14:26:19 -0400

    added routes for employee

--- Assignments/Assignment-1/routes/EmployeeRoutes.js
+++ Assignments/Assignment-1/routes/EmployeeRoutes.js
@@ -1,3 +1,67 @@
 const employeeModel = require('../models/EmployeeModel');
 const express = require('express');
-const userRoutes = express.Router();
+const employeeRoutes = express.Router();
+
+// Create Employee
+employeeRoutes.post('/employees', (req, res) => {
+    // Validate request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Fields must not be empty"
+        });
+    }
+    employee = new employeeModel(req.body.content)
+    employee.save().then(() => {
+        res.status(201).send();
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+});
+// Get all Employees
+employeeRoutes.get('/employees', (req, res) => {
+    employeeModel.find({}).exec().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+});
+// Get Employee by ID
+employeeRoutes.get('/employees/:employeeId', (req, res) => {
+    employeeModel.findById(req.params.employeeId).then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+});
+// Update Employee by ID
+employeeRoutes.put('/employees/:employeeId', (req, res) => {
+    // Validate request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Fields must not be empty"
+        });
+    }
+    employeeModel.findByIdAndUpdate(req.params.employeeId, req.body.content, { new: true }).then(data => {
+        res.status(204).send();
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+});
+// Delete Employee by ID
+employeeRoutes.delete('/employees/:employeeId', (req, res) => {
+    employeeModel.findByIdAndDelete(req.params.employeeId).then(data => {
+        res.status(204).send();
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+});
